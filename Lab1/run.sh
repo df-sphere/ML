951e953c22908dfef5188827131793d6d66d9127
Diego Franco <diego@stochastic-rnd.com>
2026-01-31 14:43:49 -0600

    Lab1: Added test.sh
    Signed-off-by: Diego Franco <diego@stochastic-rnd.com>

--- Lab1/run.sh
+++ Lab1/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#conda activate cs7643-a1
+conda activate cs7643-a1
 python3 main.py --config configs/config_exp.yaml
---
+++ Lab1/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+conda activate cs7643-a1
+python3 -m unittest tests.test_loading
+
